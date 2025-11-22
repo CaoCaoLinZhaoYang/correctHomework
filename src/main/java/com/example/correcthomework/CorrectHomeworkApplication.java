0b9f9f25793e1fa71bda868161585c412af0330d
LinZhaoYang <993773790@qq.com>
2025-11-22 14:27:53 +0800

    学生作业订正系统

---
+++ src/main/java/com/example/correcthomework/CorrectHomeworkApplication.java
@@ -0,0 +1,34 @@
+package com.example.correcthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrectHomeworkApplication.class, args);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/test/java/com/example/correcthomework/CorrectHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.correcthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
